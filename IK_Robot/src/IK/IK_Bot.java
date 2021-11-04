624e647acbd4a2b26fc9ef3b689227c0ce4d2523
Igor <rowan93@web.de>
2021-11-04 14:31:39 +0100

    Added capability to intercept robot trajectory.

--- IK_Robot/src/IK/IK_Bot.java
+++ IK_Robot/src/IK/IK_Bot.java
@@ -1,5 +1,7 @@
 package IK;
 
+import java.awt.geom.Point2D;
+
 import robocode.AdvancedRobot;
 import robocode.HitRobotEvent;
 import robocode.Rules;
@@ -10,7 +12,12 @@ public class IK_Bot extends AdvancedRobot {
 	  double previousEnergy = 100;
 	  int movementDirection = 1;
 	  int gunDirection = 1;
+	  double oldEnemyHeading;
+	  double buffer;
 	  public void run() {
+	  double width = this.getBattleFieldWidth();
+	  double height = this.getBattleFieldHeight();
+	  buffer = 0.2 * Math.max(height, width);
 		setAdjustGunForRobotTurn(true);
 	    do {
 	    	if (getRadarTurnRemaining()== 0.0) {
@@ -24,34 +31,31 @@ public class IK_Bot extends AdvancedRobot {
 	    ScannedRobotEvent e) {
 	      double angleToEnemy = getHeadingRadians() + e.getBearingRadians();
 		  double radarTurn = Utils.normalRelativeAngle(angleToEnemy - getRadarHeadingRadians());
-		  double gunTurn = Utils.normalRelativeAngle(angleToEnemy - getGunHeadingRadians());
+		  //double gunTurn = Utils.normalRelativeAngle(angleToEnemy - getGunHeadingRadians());
 		  double extraTurn = Math.min(Math.atan(36.0/e.getDistance()),Rules.RADAR_TURN_RATE_RADIANS);
 		  radarTurn += (radarTurn < 0 ? -extraTurn : extraTurn);
 		  
 		  setTurnRadarRightRadians(radarTurn);
-		  setTurnGunRightRadians(gunTurn);
+		  //setTurnGunRightRadians(gunTurn);
 	      // Stay at right angles to the opponent
 		  setTurnRight(e.getBearing()+90-30*movementDirection);
 	      
 	     // If the bot has small energy drop, it most likely fired
-	    double changeInEnergy =
-	      previousEnergy-e.getEnergy();
+	    double changeInEnergy = previousEnergy-e.getEnergy();
 	    if (changeInEnergy>0 && changeInEnergy<=3) {
 	         movementDirection =-movementDirection;
 	         setAhead((e.getDistance()/4+25)*movementDirection);
 	     }
 	    
+	    
 	    // Fire directly at target
-	    fire ( 2 ) ;
+	    if (e.getDistance()<2*buffer) firelineary ( e );
 	    
 	    // Track the energy level
 	    previousEnergy = e.getEnergy();
 	  }
 	  
 		public void onHitRobot(HitRobotEvent e) {
-			
-		      double angleToEnemy = getHeadingRadians() + e.getBearingRadians();
-			  double bodyTurn = Utils.normalRelativeAngle(angleToEnemy - getHeadingRadians());
 
 			turnRight(e.getBearing());
 
@@ -70,6 +74,38 @@ public class IK_Bot extends AdvancedRobot {
 			}
 			ahead(40); // Ram him again!
 		}
+		
+		
+		public void firelineary(ScannedRobotEvent e) {
+			double bulletPower = 3.0;
+			double myX = this.getX();
+			double myY = this.getY();
+			double absoluteBearing = e.getBearingRadians() + this.getHeadingRadians();
+			double enemyX = myX + e.getDistance() * Math.sin(absoluteBearing);
+			double enemyY = myY + e.getDistance() * Math.cos(absoluteBearing);
+			double enemyHeading = e.getHeadingRadians();
+			double enemyHeadingChange = enemyHeading - oldEnemyHeading;
+			oldEnemyHeading = enemyHeading;
+			double enemyVelocity = e.getVelocity();
+			
+			double deltaTime = 0;
+			double predictedX = enemyX;
+			double predictedY = enemyY;
+			
+			while (((++deltaTime)*(20-3.0*bulletPower))<Point2D.Double.distance(myX,myY,predictedX,predictedY)) {
+				predictedX += Math.sin(enemyHeading)*enemyVelocity;
+				predictedY += Math.cos(enemyHeading)*enemyVelocity;
+				enemyHeading += enemyHeadingChange;
+			}
+			
+			double theta = Utils.normalAbsoluteAngle(Math.atan2(predictedX-myX, predictedY-myY));
+			this.setTurnGunRightRadians(Utils.normalRelativeAngle(theta-this.getGunHeadingRadians()));
+			fire(bulletPower);
+			
+		}
+		
+		
+		
 
 }
 
